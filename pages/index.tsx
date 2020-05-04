3347472b7d32277c0cf88e8e042be71b270cb30f
Zach White <zechnwhite@gmail.com>
2020-05-04 16:40:25 -0700

    adding some pseudo code

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
+import axios from 'axios';
 
 import '../assets/styles/main.scss';
 
-interface HomeProps {
-    title?: string;
-}
+interface HomeProps {}
+
 
 function Home(props: HomeProps): JSX.Element {
+
+  const[state, setState] = useState({
+    authors: [],
+    articles: []
+  })
+  
+  useEffect(() => {
+    if(localStorage.getItem('userid')){
+      axios.get(`https://petersapi.com/userauthor/${localStorage.getItem('userid')}`)
+        .then(res => {
+          setState({
+            ...state,
+            authors: res.data.authors
+          })
+          const userauthors = res.data.authors;
+          axios.post('https://petersapi.com/getposts', userauthors)
+          .then(res => {
+            setState({
+              ...state,
+              articles: res.data.articles
+            })
+          })
+        })
+    }
+  }, [])
+
+
   return (
     <>
       <h1>Home page</h1>
